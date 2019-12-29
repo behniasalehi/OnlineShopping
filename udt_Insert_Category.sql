8d45a592385fb2a4de49640f965649b620e30432
behniasalehi <behniasalehi955@gmail.com>
2019-12-29 07:56:40 -0800

    add Insert Usp and Udt for Category

---
+++ udt_Insert_Category.sql
@@ -0,0 +1,6 @@
+﻿use OnlineShopping
+go
+Create Type dbo.udt_Insert_Category as Table(
+CategoryName nvarchar(50),
+Descriptions nvarchar(100)
+)
---
+++ usp_Insert_Category.sql
@@ -0,0 +1,18 @@
+﻿use OnlineShopping
+go
+create PROCEDURE dbo.usp_Insert_Category
+  @InsertCategory as dbo.udt_Insert_Category readonly,
+  @message nvarchar(300) output
+AS
+begin tran 
+begin try
+insert into dbo.Category(CategoryName , Descriptions)
+select * from @InsertCategory
+set @message='Insert is done successfully.'
+commit tran 
+end try
+begin catch 
+set @message='Unsuccessful' + ERROR_MESSAGE();
+rollback tran 
+end catch 
+go
